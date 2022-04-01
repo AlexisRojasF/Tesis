86d8876df3524788cba535baaba214148e58aa1e
alexis <alexisrojasf@outlook.es>
2022-03-31 22:44:31 -0500

    se arreglo el bug del loggin2

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -49,10 +49,10 @@ const login = async (req = request, res = response) => {
         const token = await generarJWT(usuario.id);
 
         if (usuario.rol == "PROFESOR_ROLE") {
-            crearProfesor
+            crearProfesor(request, response);
         
         } else if (usuario.rol == "ESTUDIANTE_ROLE"){
-            crearEstudiante
+            crearEstudiante(request, response);
         }
 
         res.json({
